3ad095a289d3de5a429bac2e91face54af0a7e73
Joshua Gregory <yoshig5114@gmail.com>
2023-06-05 18:16:22 -0400

    done

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+const submitButton = document.querySelector("#submitSearch")
+let searchInput = document.querySelector("#searchWord")
+const imgElement = document.querySelector("#image")
+const feedbackPar = document.querySelector("#feedbackParagraph")
+
+// Exercise 2
+const apiKey = "wyCA4MYBH9Wyq6rYzEUSh8BVCMTl07qQ"
+submitButton.addEventListener("click", () => {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${searchInput.value}`, {mode: "cors"})
+
+    .then((response) => {
+        return response.json()
+        console.log(response)
+    })
+    .then((res) => {
+        imgElement.src = res.data.images.original.url 
+        searchInput.value = ""
+    })
+    .catch((err) =>{
+        console.log(err)
+        feedbackPar.textContent = err.message
+    })
+})
